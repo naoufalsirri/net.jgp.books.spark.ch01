638badec54ba9863996226050acc6aae2b7711e4
Naoufal <naoufal.sr10@gmail.com>
2023-04-19 20:38:01 +0200

    commit un dev2

---
+++ src/main/java/net/jgp/books/spark/ch01/lab100_csv_to_dataframe/AnotherClass.java
@@ -0,0 +1,5 @@
+package net.jgp.books.spark.ch01.lab100_csv_to_dataframe;
+
+public class AnotherClass {
+    private String nom;
+}
---
+++ src/main/java/net/jgp/books/spark/ch01/lab100_csv_to_dataframe/YourClass.java
@@ -0,0 +1,4 @@
+package net.jgp.books.spark.ch01.lab100_csv_to_dataframe;
+
+public class YourClass {
+}
